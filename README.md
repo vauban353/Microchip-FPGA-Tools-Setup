cc727e0c9dae685f2b3868c23d6032ad260c81bb
vauban353 <vauban353@gmail.com>
2022-03-27 21:21:48 +0100

    Initial version.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Microchip-FPGA-Tools-Setup
+
+This script is intended for use on Ubuntu to setup environment variables allowing command line development for PolarFire SoC.
+
+## Executing the script
+
+This script needs to be sourced. Do not forget the leading dot.
+
+```
+. ./setup-microchip-tools.sh
+```
+
+## Adjusting the script for your installation
+By default the script assumes that the tools are installed in your home directory as follows:
+
+```
+  Home
+    |
+    |-Microchip
+        |
+        |- Libero_SoC_v2021.3
+        |- license
+        |- Linux_Licensing_Daemon
+        |- SoftConsole-v2021.3-7.0.0.599
+```
+
+Edit the top of the script to adjust installation directory names if the tools are installed in other locations on your machine or if you are using different versions of the tools.
---
+++ setup-microchip-tools.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#===============================================================================
+# Edit the following section with the location where the following tools are
+# installed:
+#   - SoftConsole (SC_INSTALL_DIR)
+#   - Libero (LIBERO_INSTALL_DIR)
+#   - Licensing daemon for Libero (VOMIT_INSTALL_DIR)
+#===============================================================================
+export SC_INSTALL_DIR=/home/$USER/Microchip/SoftConsole-v2021.3-7.0.0.599
+export LIBERO_INSTALL_DIR=/home/$USER/Microchip/Libero_SoC_v2021.3
+export VOMIT_INSTALL_DIR=/home/$USER/Microchip/Linux_Licensing_Daemon
+export LICENSE_FILE_DIR=/home/$USER/Microchip/license
+
+#===============================================================================
+# The following was tested on Ubuntu 20.04 with:
+#   - Libero 2021.3
+#   - SoftConsole 2021.3
+#===============================================================================
+
+#
+# SoftConsole
+#
+export PATH=$PATH:SC_INSTALL_DIR/riscv-unknown-elf-gcc/bin
+export FPGENPROG=LIBERO_INSTALL_DIR/Libero/bin64/fpgenprog
+
+#
+# Libero
+#
+export PATH=$PATH:$LIBERO_INSTALL_DIR/Libero/bin:$LIBERO_INSTALL_DIR/Synplify/bin:
+export PATH=$PATH:$LIBERO_INSTALL_DIR/Model/modeltech/linuxacoem
+export LOCALE=C
+export LD_LIBRARY_PATH=/usr/lib/i386-linux-gnu/
+
+#
+# Libero License daemon
+#
+export LM_LICENSE_FILE=1702@localhost
+export SNPSLMD_LICENSE_FILE=1702@localhost
+
+$VOMIT_INSTALL_DIR/lmgrd -c $LICENSE_FILE_DIR/License.dat -l $LICENSE_FILE_DIR/license.log
